aab3368927cf3924db5e3c32a929b4be48d7eda1
BRANDON PARKER <Brandon@dkparker.com>
2016-03-07 21:19:44 -0800

    Starting to add in a lot of content and baseline for portfolio

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+var liveProjects = [];
+
+function Project (opts) {
+  this.title = opts.title;
+  this.author = opts.author;
+  this.authorUrl = opts.authorUrl;
+  this.publishedOn = opts.publishedOn;
+  this.body = opts.body;
+  this.imgLink = opts.imgLink;
+}
